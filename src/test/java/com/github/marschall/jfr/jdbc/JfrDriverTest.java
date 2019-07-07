47722add62ab3fe04b84a1e1f9dc8d6a7fba3ccb
Philippe Marschall <philippe.marschall@gmail.com>
2019-07-07 12:23:23 +0200

    test for metadata

--- src/test/java/com/github/marschall/jfr/jdbc/JfrDriverTest.java
+++ src/test/java/com/github/marschall/jfr/jdbc/JfrDriverTest.java
@@ -1,8 +1,11 @@
 package com.github.marschall.jfr.jdbc;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
 import org.junit.jupiter.api.BeforeAll;
@@ -17,9 +20,19 @@ class JfrDriverTest {
 
   @Test
   void connect() throws SQLException {
+    try (var connection = DriverManager.getConnection("jdbc:jfr:h2:mem:")) {
+      DatabaseMetaData metaData = connection.getMetaData();
+      assertFalse(metaData.isReadOnly());
+    }
+  }
+
+  @Test
+  void preparedStatement() throws SQLException {
     try (var connection = DriverManager.getConnection("jdbc:jfr:h2:mem:");
-         var preparedStatement = connection.prepareStatement("SELECT 1");
-         var resultSet = preparedStatement.executeQuery()) {
+            var preparedStatement = connection.prepareStatement("SELECT 1");
+            var resultSet = preparedStatement.executeQuery()) {
+      ResultSetMetaData metaData = resultSet.getMetaData();
+      assertEquals(1, metaData.getColumnCount());
       while (resultSet.next()) {
         assertEquals(1, resultSet.getInt(1));
       }
