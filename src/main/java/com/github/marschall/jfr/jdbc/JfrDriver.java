ceb57753f8aafddeb663ba1c586e10393ba8b414
Marius Volkhart <marius@volkhart.com>
2024-02-02 17:32:02 -0500

    Instrument connections established using the Driver

--- src/main/java/com/github/marschall/jfr/jdbc/JfrDriver.java
+++ src/main/java/com/github/marschall/jfr/jdbc/JfrDriver.java
@@ -36,9 +36,17 @@ public final class JfrDriver implements Driver {
   @Override
   public Connection connect(String url, Properties info) throws SQLException {
     if (!this.acceptsURL(url)) {
-      throw new SQLException("unvalid url: " + url);
+      throw new SQLException("invalid url: " + url);
     }
-    return DriverManager.getConnection("jdbc:" + url.substring(9), info);
+    var event = new JdbcOperationEvent();
+    event.operationObject = "DataSource";
+    event.operationName = "connect";
+    try {
+      return new JfrConnection(DriverManager.getConnection("jdbc:" + url.substring(9), info));
+    } finally {
+      event.end();
+      event.commit();
+    }
   }
 
   @Override
